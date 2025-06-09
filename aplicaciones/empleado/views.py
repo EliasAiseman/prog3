fc5dad9726919099947785f41f9dad3af1bfb935
Elias <98857479+EliasAiseman@users.noreply.github.com>
2025-06-08 21:28:59 -0300

    modificaciones semana4

--- aplicaciones/empleado/views.py
+++ aplicaciones/empleado/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 # Create your views here.
 
+class Vistas (TemplateView):
+  template_name= 'empleado/home.html'
 
+class Lista(ListView):
+  template_name= 'empleado/lista.html'
+  queryset=['Elias','Aiseman','Analista De Sistemas']
+  context_object_name= 'lista'
